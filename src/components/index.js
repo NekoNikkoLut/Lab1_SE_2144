602fb95afffabea7bfe5dea0daeda3ffa5c5f0ec
Nikko <neilnikkomarianno.lut-24@cpu.edu.ph>
2026-07-31 23:02:25 +0800

    fix: remove duplicate js files conflicting with ts sources

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { CartDrawer } from './CartDrawer';
-export { CartItem } from './CartItem';
-export { Filters } from './Filters';
-export { Header } from './Header';
-export { ProductCard } from './ProductCard';
-export { ProductGrid } from './ProductGrid';
